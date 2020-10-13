42b32683f2863c3051c9f5f3a6d0d99fca18b778
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-13 15:02:52 -0700

    add first function for #1

--- advanced-looping-exercise.js
+++ advanced-looping-exercise.js
@@ -33,7 +33,13 @@ const array2 = ['a', 3, 4, 2] // should return 4
 const array3 = [] // should return 0
 
 function biggestNumberInArray(arr) {
-
+  let biggestNum = 0
+  for (let i = 0; i < arr.length; i++ ) {
+    if (arr[i] > biggestNum) {
+      biggestNum = arr[i]
+    }
+  }
+  return biggestNum
 }
 
 function biggestNumberInArray2(arr) {
